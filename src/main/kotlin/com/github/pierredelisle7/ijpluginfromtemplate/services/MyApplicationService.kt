631742549bb51fdb0466a89a731970ce9478eb32
GitHub Action <action@github.com>
2021-09-28 01:32:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pierredelisle7/ijpluginfromtemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pierredelisle7.ijpluginfromtemplate.services
+
+import com.github.pierredelisle7.ijpluginfromtemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/pierredelisle7/ijpluginfromtemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pierredelisle7/ijpluginfromtemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pierredelisle7.ijpluginfromtemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pierredelisle7.ijpluginfromtemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
